ebe2588e00fc42c4256fd3aab2236623a1da0ea3
Ryan Andonian <ryanandonian@gmail.com>
2018-04-20 20:41:03 -0700

    Adds basic movement to player using debug buttons

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 kCameraOffset = new Vector3(0, 3, -10);
+    private GameObject mPlayer;
+    private Vector3 mVelocity = Vector3.zero;
+
+    // Use this for initialization
+    private void Start()
+    {
+        mPlayer = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Move toward the player
+        Vector3 targetPosition = (mPlayer.transform.position + kCameraOffset);
+        Debug.DrawLine(transform.position, targetPosition, Color.yellow);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mVelocity, 0.3f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+
+{
+    LEFT = 0,
+    RIGHT = 1
+}
+
+public enum PlayerState
+{
+    RUNNING,
+    IDLE
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody body;
+    public float kRunSpeed = 1.0f;
+
+    private float mRunStart;
+    private float mRunDuration;
+    private Direction mRunDir;
+    private PlayerState mState = PlayerState.IDLE;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Log("Hello player here");
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void DoRun()
+    {
+        // We're outta running time
+        if (mRunStart + mRunDuration < Time.time)
+        {
+            mState = PlayerState.IDLE;
+        }
+        else
+        {
+            Vector3 newPosition = transform.position;
+            switch (mRunDir)
+            {
+                case Direction.LEFT:
+                    newPosition = transform.position + Vector3.left * kRunSpeed * Time.deltaTime;
+                    break;
+                case Direction.RIGHT:
+                    newPosition = transform.position + Vector3.right * kRunSpeed * Time.deltaTime;
+                    break;
+            }
+            body.MovePosition(newPosition);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (mState == PlayerState.RUNNING)
+        {
+            DoRun();
+        }
+    }
+
+    #region User Actions
+
+    public void ActionJump()
+    {
+        Debug.Log("Player Jumpin");
+        body.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+    }
+
+    public void ActionRunLeft(float dist)
+    {
+        ActionRun(Direction.LEFT, dist);
+    }
+
+    public void ActionRunRight(float dist)
+    {
+        ActionRun(Direction.RIGHT, dist);
+    }
+
+    private void ActionRun(Direction dir, float dist)
+    {
+        mState = PlayerState.RUNNING;
+        mRunDir = dir;
+        mRunStart = Time.time;
+        mRunDuration = dist;
+    }
+
+    #endregion User Actions
+}
