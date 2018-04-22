ed53404daaddb243cd353387e727d86ae037835d
Ryan Andonian <ryanandonian@gmail.com>
2018-04-22 02:26:43 -0700

    Camera jitter fix by splitting to FixedUpdate/LateUpdate Closes #16

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,12 +14,27 @@ public class CameraController : MonoBehaviour
         mPlayer = GameObject.Find("Player");
     }
 
+    private void FixedUpdate()
+    {
+        // For some reason this works by splitting up Y and X/Z movement for the camera so #CLAMJAM
+        // Move toward the player
+        Vector3 targetPosition = (mPlayer.transform.position + kCameraOffset);
+        Debug.DrawLine(transform.position, targetPosition, Color.yellow);
+        Vector3 moveEase = (targetPosition - transform.position) * 0.25f;
+        moveEase.y = 0;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mVelocity, 0.1f);
+        transform.Translate(moveEase);
+    }
+
     // Update is called once per frame
-    private void Update()
+    private void LateUpdate()
     {
         // Move toward the player
         Vector3 targetPosition = (mPlayer.transform.position + kCameraOffset);
         Debug.DrawLine(transform.position, targetPosition, Color.yellow);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mVelocity, 0.1f);
+        Vector3 moveEase = (targetPosition - transform.position) * 0.25f;
+        moveEase.x = moveEase.z = 0;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mVelocity, 0.1f);
+        transform.Translate(moveEase);
     }
 }
