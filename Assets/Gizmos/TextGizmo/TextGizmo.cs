89bff96a97fe9f6d1fffdca98288c194c6ce756b
Ryan Andonian <ryanandonian@gmail.com>
2018-04-21 19:38:50 -0700

    Fixes silly directive for the textgizmos

--- Assets/Gizmos/TextGizmo/TextGizmo.cs
+++ Assets/Gizmos/TextGizmo/TextGizmo.cs
@@ -4,91 +4,90 @@
  *   Carl Emil Carlsen
  *   sixthsensor.dk
  */
-#if UNITY_EDITOR
+
 using UnityEngine;
 using System.Collections.Generic;
 
 public class TextGizmo
 {
-	#region Singleton
-	private class Singleton
-	{
-		static Singleton()
-		{
-			if (instance == null)
-			{
-				instance = new TextGizmo();
-			}
-		}
+    #region Singleton
 
-		internal static readonly TextGizmo instance;
-	}
+    private class Singleton
+    {
+        static Singleton()
+        {
+            if (instance == null)
+            {
+                instance = new TextGizmo();
+            }
+        }
 
-	public static TextGizmo Instance { get { return Singleton.instance; } }
-	
-	#endregion
-	
-	private const int CHAR_TEXTURE_HEIGHT = 11;
-	private const int CHAR_TEXTURE_WIDTH = 8;
-	private const string characters = " !#%'()+,-.0123456789;=abcdefghijklmnopqrstuvwxyz_{}~\\?\":/*";
-	
-	private Dictionary<char, string> texturePathLookup;
-	private Dictionary<char, string> specialChars;
+        internal static readonly TextGizmo instance;
+    }
 
+    public static TextGizmo Instance { get { return Singleton.instance; } }
 
+    #endregion Singleton
 
-	private TextGizmo()
-	{
+    private const int CHAR_TEXTURE_HEIGHT = 11;
+    private const int CHAR_TEXTURE_WIDTH = 8;
+    private const string characters = " !#%'()+,-.0123456789;=abcdefghijklmnopqrstuvwxyz_{}~\\?\":/*";
 
-		specialChars = new Dictionary<char, string>();
+    private Dictionary<char, string> texturePathLookup;
+    private Dictionary<char, string> specialChars;
 
-		specialChars.Add('\\', "backslash");
-		specialChars.Add('?', "questionmark");
-		specialChars.Add('"', "quotes");
-		specialChars.Add(':', "colon");
-		specialChars.Add('/', "slash");
-		specialChars.Add('*', "star");
+    private TextGizmo()
+    {
+        specialChars = new Dictionary<char, string>();
 
-		texturePathLookup = new Dictionary<char, string>();
+        specialChars.Add('\\', "backslash");
+        specialChars.Add('?', "questionmark");
+        specialChars.Add('"', "quotes");
+        specialChars.Add(':', "colon");
+        specialChars.Add('/', "slash");
+        specialChars.Add('*', "star");
 
-		for (int c = 0; c < characters.Length; c++)
-		{
-			string charName = specialChars.ContainsKey(characters[c]) ? specialChars[characters[c]] : characters[c].ToString();
-			texturePathLookup.Add(characters[c], "TextGizmo/text_" + charName + ".png");
-		}
-	}
+        texturePathLookup = new Dictionary<char, string>();
 
+        for (int c = 0; c < characters.Length; c++)
+        {
+            string charName = specialChars.ContainsKey(characters[c]) ? specialChars[characters[c]] : characters[c].ToString();
+            texturePathLookup.Add(characters[c], "TextGizmo/text_" + charName + ".png");
+        }
+    }
 
+    public void DrawText(Vector3 position, object message)
+    {
+        DrawText(position, message != null ? message.ToString() : "(null)");
+    }
 
-	public void DrawText(Vector3 position, object message) { DrawText(position, message != null ? message.ToString() : "(null)"); }
+    public void DrawText(Vector3 position, string format, params object[] args)
+    {
+        DrawText(position, string.Format(format, args));
+    }
 
-	public void DrawText(Vector3 position, string format, params object[] args) { DrawText(position, string.Format(format, args)); }
+    private void DrawText(Vector3 position, string text)
+    {
+        // Set it up to be the current camera
+        Camera camera = Camera.current;
+        string lowerText = text.ToLower();
 
+        Vector3 screenPoint = camera.WorldToScreenPoint(position);
 
-
-	private void DrawText(Vector3 position, string text)
-	{
-		// Set it up to be the current camera
-		Camera camera = Camera.current;
-		string lowerText = text.ToLower();
-
-		Vector3 screenPoint = camera.WorldToScreenPoint(position);
-
-		Vector3 offset = Vector3.zero;
-		for (int c = 0, n = lowerText.Length; c < n; ++c)
-		{
-			if ('\n'.Equals(lowerText[c]))
-			{
-				offset.y += CHAR_TEXTURE_HEIGHT + 2;
-				offset.x = 0;
-				continue;
-			}
-			else if (texturePathLookup.ContainsKey(lowerText[c]))
-			{
-				Gizmos.DrawIcon(camera.ScreenToWorldPoint(screenPoint + offset), texturePathLookup[lowerText[c]]);
-				offset.x += CHAR_TEXTURE_WIDTH;
-			}
-		}
-	}
+        Vector3 offset = Vector3.zero;
+        for (int c = 0, n = lowerText.Length; c < n; ++c)
+        {
+            if ('\n'.Equals(lowerText[c]))
+            {
+                offset.y += CHAR_TEXTURE_HEIGHT + 2;
+                offset.x = 0;
+                continue;
+            }
+            else if (texturePathLookup.ContainsKey(lowerText[c]))
+            {
+                Gizmos.DrawIcon(camera.ScreenToWorldPoint(screenPoint + offset), texturePathLookup[lowerText[c]]);
+                offset.x += CHAR_TEXTURE_WIDTH;
+            }
+        }
+    }
 }
-#endif
