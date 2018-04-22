82110a246efa019dc6be501e0dd1c98fb8fca433
Ryan Andonian <ryanandonian@gmail.com>
2018-04-22 02:13:14 -0700

    Adds MOVING PLATFORMS! (Closes #27) Adds a neat finishline. Right now it just opens up a stupid twitter link. That needs a fresh WIN screen.

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    private GameController mGameController;
+    private Renderer mRenderer;
+
+    // Use this for initialization
+    private void Start()
+    {
+        mRenderer = GetComponent<Renderer>();
+        mGameController = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 offset = mRenderer.material.mainTextureOffset;
+        offset.x += 0.01f;
+        mRenderer.material.mainTextureOffset = offset;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player Completed the game");
+        Application.OpenURL("https://twitter.com/intent/tweet?text=I%20can't%20believe%20I%20beat%20%40zambini845%20's%20ridiculous%20game&hashtags=LDJAM%2CLDJAM41");
+    }
+}
---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlatformState
+{
+    WAITING,
+    MOVING
+}
+
+public class MovingPlatformController : MonoBehaviour
+{
+    private Rigidbody mBody;
+
+    private PlatformState mState = PlatformState.MOVING;
+    private Vector3 mPathStart;
+    public Vector3 mPathEnd;
+    public float kMoveSpeed = 5.0f;
+    private Vector3 mVelocity = Vector3.zero;
+    private bool isForward = true;
+
+    private Vector3 TargetPosition
+    { get { return (isForward) ? mPathEnd : mPathStart; } }
+
+    // Use this for initialization
+    private void Start()
+    {
+        mPathStart = transform.position;
+        mBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (mState)
+        {
+            case PlatformState.MOVING:
+                // Bounce if moving
+                if ((TargetPosition - transform.position).sqrMagnitude < 0.0125f)
+                {
+                    mBody.MovePosition(TargetPosition);
+                    isForward = !isForward;
+                    mState = PlatformState.WAITING;
+                    Invoke("ResumeMovement", 2.0f);
+                }
+                break;
+        }
+    }
+
+    private void ResumeMovement()
+    {
+        mState = PlatformState.MOVING;
+    }
+
+    private void FixedUpdate()
+    {
+        if (mState == PlatformState.MOVING)
+        {
+            // Move toward the player
+            Vector3 targetPosition = TargetPosition;
+            Debug.DrawLine(transform.position, targetPosition, Color.yellow);
+            Vector3 nextStepDir = (targetPosition - transform.position).normalized;
+            Vector3 nextStepPosition = transform.position + nextStepDir * Time.deltaTime * kMoveSpeed;
+            mBody.MovePosition(nextStepPosition);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(mPathEnd, transform.localScale);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Debug.DrawLine(transform.position, mPathEnd, Color.magenta);
+        Debug.DrawLine(transform.position, TargetPosition, Color.green);
+        Gizmos.DrawSphere(mPathEnd, 0.1f);
+    }
+}
